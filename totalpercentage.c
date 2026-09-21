86e9b5339460e69717790cea283de56f553e167b
nidhielizabeth <nidhijino@gmail.com>
2026-09-21 11:55:16 +0530

    Add files via upload

---
+++ totalpercentage.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    float m1, m2, m3, m4, m5, total, percentage;
+    printf("Enter marks for 5 subjects: ");
+    scanf("%f %f %f %f %f", &m1, &m2, &m3, &m4, &m5);
+    total = m1 + m2 + m3 + m4 + m5;
+    percentage = (total / 500.0)*100.0;
+    printf("total = %.2f, Percentage = %2f%%\n", total, percentage);
+    return 0;}
+
